433f77b7d265db3a14fa7c62d8dffd50e9af83b5
Shan Chunqing <vxst@vxst.org>
2022-05-26 09:55:35 +0700

    Add models, and license

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Proj Demo
 
 A demo of SemVer and Jenkins CI/CD
+
+## Pastebin
+An app to manage large texts.
--- pastebin/views.py
+++ pastebin/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
-# Create your views here.
+
+def add(request):
+    paste_data = json.loads(request.body)
+    title = paste_data['title']
+    content = paste_data['content']
